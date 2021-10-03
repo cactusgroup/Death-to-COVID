1a7fe7d5ace4c98d0543e228a0ecd4c9e8aa36f3
unknown <joshuachu007@gmail.com>
2021-10-02 21:54:42 -0400

    Testing pygame and spyder. see test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  2 21:43:56 2021
+
+@author: Joshua
+"""
+
+import pygame
+
+background_color = (255,255,255)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Tutorial 1')
+screen.fill(background_color)
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
